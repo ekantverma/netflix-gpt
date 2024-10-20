92190968c6e8cd6ae45aad1b31b708e48026a52f
Ekant Verma <ekantvermasolanki@gmail.com>
2024-10-20 18:02:04 +0530

    Fix UI of title

--- src/components/VedioTitle.js
+++ src/components/VedioTitle.js
@@ -2,15 +2,25 @@ import React from "react";
 
 const VedioTitle = ({ title, overview }) => {
   return (
-    <div className="w-full aspect-video absolute text-white bg-gradient-to-r from-black to-transparent">
-      <div className="absolute inset-0 flex flex-col justify-center px-8 md:px-24">
-        <h1 className="text-4xl md:text-6xl font-bold">{title}</h1>
-        <p className="py-6 text-base md:text-lg md:w-1/2 lg:w-1/4">{overview}</p>
+    <div className="absolute top-10 sm:top-2 md:-top-5 left-0 z-20 flex flex-col justify-center bottom-0 w-full pl-4 sm:pl-6 md:pl-10 aspect-video bg-gradient-to-t from-black from-10%">
+      <div className="w-2/3 sm:w-1/2 md:w-1/3">
+        <h1
+          className="text-xl sm:text-3xl md:text-4xl font-bold mb-2 whitespace-nowrap text-white"
+          style={{ filter: "drop-shadow(2px 4px 1.5px black)" }}
+        >
+          {title}
+        </h1>
+        <p
+          className="hidden sm:visible max-h-24 h-fit sm:line-clamp-2 md:line-clamp-4 mb-3 text-sm text-white"
+          style={{ filter: "drop-shadow(2px 4px 1.5px black)" }}
+        >
+          {overview}
+        </p>
         <div className="flex gap-4">
-          <button className="bg-white text-black p-3 md:p-4 px-6 md:px-12 text-lg md:text-xl rounded-lg hover:bg-opacity-80">
+          <button className="my-4 px-2 sm:px-4 md:px-6  py-1 sm:py-2  bg-white text-black rounded-md cursor-pointer border border-black font-semibold sm:text-base md:text-lg text-sm hover:bg-opacity-80">
             &#9654; Play
           </button>
-          <button className="bg-gray-500 text-white p-3 md:p-4 px-6 md:px-12 text-lg md:text-xl bg-opacity-50 rounded-lg">
+          <button className="my-4 px-2 sm:px-4 md:px-6  py-1 sm:py-2  bg-gray-500  text-black rounded-md cursor-pointer border border-black font-semibold sm:text-base md:text-lg text-sm bg-opacity-50">
             More Info
           </button>
         </div>
