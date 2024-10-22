abc748eb57b643eedce5e8b18ec8ee886b12f6fa
Ekant Verma <ekantvermasolanki@gmail.com>
2024-10-22 19:29:05 +0530

    Updated Sequence of movie

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -8,8 +8,8 @@ const SecondaryContainer = () => {
     <div className="bg-black">
       <div className="-mt-52 relative z-20 pl-6">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Popular"} movies={movies.nowPlayingMovies} />
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
       </div>
